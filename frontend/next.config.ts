9e91aa8c70bbfa56b9b78c08dbd9a23da520324e
Alexander Ivanitskiy <negalapetrovna77@gmail.com>
2025-07-08 23:28:16 +0300

    eslint disable

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
